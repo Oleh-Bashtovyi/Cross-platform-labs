17a732450156ceb5566cdb1457776385edee7cdc
Oleh <olehbashtovyi@gmail.com>
2024-09-14 12:15:09 +0300

    add lab 1 project

---
+++ Lab_1/Lab_1/Order.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab_1;
+
+public class Order
+{
+    public int Deadline {  get; set; }
+    public int Profit {  get; set; }
+
+    public Order() { }
+
+    public Order(int deadline, int profit)
+    {
+        Deadline = deadline;
+        Profit = profit;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Order other)
+        {
+            return Deadline == other.Deadline && Profit == other.Profit;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
---
+++ Lab_1/Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine($"File not found: {filePath}");
+                return;
+            }
+
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
