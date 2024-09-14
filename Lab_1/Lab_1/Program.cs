49537a71fe642ccfee0be6f84031a570da8b1748
Oleh <olehbashtovyi@gmail.com>
2024-09-14 15:19:46 +0300

    add launch code

--- Lab_1/Lab_1/Program.cs
+++ Lab_1/Lab_1/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            var outputFile = "output.txt";
+
+            string? inputFilePath;
+
+            if (args.Length > 0)
+            {
+                inputFilePath = args[0];
+            }
+            else
+            {
+                //relative path
+                inputFilePath = "input.txt";
+            }
+
+            try
+            {
+                var orders = OrdersReader.Read(inputFilePath);
+
+                var solution = OrdersProblemSolver.Solve(orders);
+
+                var savePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, outputFile);
+
+                File.WriteAllText(savePath, solution.ToString());
+
+                Console.WriteLine($"Solution was successfuly written to: {savePath}");
+            }
+            catch (Exception ex)
+            {
+                var prevColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = prevColor;
+            }
+            Console.WriteLine("Program finished!");
         }
     }
 }
