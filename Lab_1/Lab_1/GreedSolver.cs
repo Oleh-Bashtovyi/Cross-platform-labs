e7055c7d5d87a66db10dbd6f4b24fe7c7647ff9d
Oleh <olehbashtovyi@gmail.com>
2024-09-14 12:47:22 +0300

    add orders problem solver method

---
+++ Lab_1/Lab_1/GreedSolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1;
+
+public static class GreedSolver
+{
+    public static int SolveOrdersProblem(IEnumerable<Order> orders)
+    {
+        if (orders.Count() == 0)
+        {
+            return 0;
+        }
+
+        var sortedOrders = orders.OrderByDescending(x => x.Reward).ToList();
+
+        var occupied = new bool[100_001];
+
+        var totalReward = 0;
+
+        foreach(var order in sortedOrders)
+        {
+            // find the nearest free day before the deadline
+            for (int day = order.Deadline; day > 0; day--)
+            {
+                if (!occupied[day])
+                {
+                    occupied[day] = true; 
+                    totalReward += order.Reward;
+                    break;
+                }
+            }
+        }
+
+        return totalReward;
+    }
+
+}
--- Lab_1/Lab_1/Order.cs
+++ Lab_1/Lab_1/Order.cs
@@ -3,21 +3,21 @@
 public class Order
 {
     public int Deadline {  get; set; }
-    public int Profit {  get; set; }
+    public int Reward {  get; set; }
 
     public Order() { }
 
     public Order(int deadline, int profit)
     {
         Deadline = deadline;
-        Profit = profit;
+        Reward = profit;
     }
 
     public override bool Equals(object? obj)
     {
         if (obj is Order other)
         {
-            return Deadline == other.Deadline && Profit == other.Profit;
+            return Deadline == other.Deadline && Reward == other.Reward;
         }
 
         return false;
