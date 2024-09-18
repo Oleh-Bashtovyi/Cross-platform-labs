7219e7d6950368a4258c42f9f244c81835c1c605
Oleh <olehbashtovyi@gmail.com>
2024-09-19 01:28:15 +0300

    README edited for root, add READ me to lab_1 folder. Add 2 tests

--- Lab_1/Lab_1_Test/OrdersProblemSolverTest.cs
+++ Lab_1/Lab_1_Test/OrdersProblemSolverTest.cs
@@ -51,30 +51,13 @@ public class OrdersProblemSolverTest(ITestOutputHelper output)
                           $"max reward = {expectedResult}");
     }
 
-/*
-    [Theory]
-    [InlineData("test_1.txt", 650)]
-    [InlineData("test_2.txt", 200000000)]
-    [InlineData("test_3.txt", 6275)]
-    [InlineData("test_4.txt", 20010)]
-    [InlineData("test_5.txt", 32750)]
-    public void SolveOrdersProblem_ReadAndSolve(string fileName, int expectedResult)
-    {
-        var filePath = Path.Combine("TestData", fileName);
-
-        var orders = IOHandler.ReadOrders(filePath);
-
-        var actualResult = OrdersProblemSolver.Solve(orders);
-
-        Assert.Equal(expectedResult, actualResult);
-    }*/
-
 
 
     [Theory]
     [InlineData(650, new int[] { 2, 100, 1, 50, 2, 150, 3, 200, 2, 300 })]
     [InlineData(200_000_000, new int[] { 5, 100_000_000, 5, 50_000_000, 5, 30_000_000, 5, 20_000_000 })]
-    public void SolveOrdersProblem_ReadAndSolve(int expectedResult, int[] data)
+    [InlineData(330, new int[] {3, 20, 4, 50, 3, 50, 2, 60, 1, 30, 4, 120, 3, 100})]
+    public void Solve_SimpleCases(int expectedResult, int[] data)
     {
         //Arrange
         var orders = new List<Order>();
@@ -90,5 +73,35 @@ public class OrdersProblemSolverTest(ITestOutputHelper output)
 
         //Assert
         Assert.Equal(expectedResult, actualResult);
+        _output.WriteLine($"Simple case: {string.Join(", ", orders)};");
+        _output.WriteLine($"Expected: {expectedResult};");
+        _output.WriteLine($"Actual: {actualResult};");
+    }
+
+
+    [Fact]
+    public void Solve_MaxNumberOfOrdersWithMaxReward()
+    {
+        var maxNumberOfOrders = 1000;
+        var maxReward = 100_000;
+        var orders = new List<Order>();
+
+        for(int i = 0; i < maxNumberOfOrders; i++)
+        {
+            var order = new Order(maxNumberOfOrders, maxReward);
+            orders.Add(order);
+        }
+
+        var expectedResult = maxNumberOfOrders * maxReward;
+
+        var actualResult = OrdersProblemSolver.Solve(orders);
+
+        Assert.Equal(expectedResult, actualResult);
+        _output.WriteLine($"Max number of orders with max deadline and rewards test case;");
+        _output.WriteLine($"Number of orders: {maxNumberOfOrders};");
+        _output.WriteLine($"Max deadline: {maxNumberOfOrders};");
+        _output.WriteLine($"Max reward: {maxReward};");
+        _output.WriteLine($"Expected: {expectedResult};");
+        _output.WriteLine($"Actual: {actualResult};");
     }
 }
