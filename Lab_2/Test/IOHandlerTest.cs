e36fa6fd5f4aad237795cfd5d69380380dabf693
Oleh <olehbashtovyi@gmail.com>
2024-09-24 00:09:06 +0300

    Add tests for lab 2

---
+++ Lab_2/Test/IOHandlerTest.cs
@@ -0,0 +1,22 @@
+﻿using App;
+
+namespace Test;
+
+public class IOHandlerTest
+{
+    [Fact]
+    public void ReadOrders()
+    {
+        var expectedResult = new ProductBlock[]
+        {
+            new(34, 29),
+            new(29, 4),
+            new(4, 15)
+        };
+
+        var filePath = Path.Combine("TestData", "test_reading.txt");
+        var actualResult = IOHandler.ReadProductBlocks(filePath);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ Lab_2/Test/ProductBlockTest.cs
@@ -0,0 +1,31 @@
+﻿using App;
+
+namespace Test;
+
+public class ProductBlockTest
+{
+    [Fact]
+    public void Equal_True()
+    {
+        var instance_1 = new ProductBlock(1, 15);
+        var instance_2 = new ProductBlock(1, 15);
+        Assert.Equal(instance_1, instance_2);
+    }
+
+    [Fact]
+    public void Equal_False()
+    {
+        var instance_1 = new ProductBlock(1, 15);
+        var instance_2 = new ProductBlock(1, 25);
+        Assert.NotEqual(instance_1, instance_2);
+    }
+
+    [Fact]
+    public void ProductBlock_ToString()
+    {
+        var instance = new ProductBlock(1, 15);
+        var actual = instance.ToString();
+        var expected = "[1, 15]";
+        Assert.Equal(expected, actual); 
+    }
+}
