2f703749c17418e3cbe0b020c0ee23dfa1d769b6
Oleh <olehbashtovyi@gmail.com>
2024-09-28 01:31:38 +0300

    add validator to lab_2

---
+++ Lab_2/App/ProductBlockValidator.cs
@@ -0,0 +1,44 @@
+﻿namespace App;
+
+public class ProductBlockValidator
+{
+    public readonly int MinLeft;
+    public readonly int MaxLeft;
+    public readonly int MinRight;
+    public readonly int MaxRight;
+
+    public ProductBlockValidator(int minLeft, int maxLeft, int minRight, int maxRight)
+    {
+        MinLeft = minLeft;
+        MaxLeft = maxLeft;
+        MinRight = minRight;
+        MaxRight = maxRight;
+    }
+
+
+    public void Validate(IEnumerable<ProductBlock> blocks)
+    {
+        foreach (var block in blocks)
+        {
+            Validate(block);
+        }
+    }
+
+    public void Validate(ProductBlock block)
+    {
+        if (block.LeftPart < MinLeft || block.LeftPart > MaxLeft)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(block),
+                $"Left part should be between {MinLeft} and {MaxLeft}." + Environment.NewLine +
+                $"Actual reward: {block.LeftPart}, product block: {block}");
+        }
+        if (block.RightPart < MinRight || block.RightPart > MaxRight)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(block),
+                $"Right part should be between {MinRight} and {MaxRight}." + Environment.NewLine +
+                $"Actual deadline: {block.RightPart}, product block: {block}");
+        }
+    }
+}
