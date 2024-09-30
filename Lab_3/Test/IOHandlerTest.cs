e0b9a5fa0bd1452a3a1471b4647f16c0c221696f
Oleh <olehbashtovyi@gmail.com>
2024-09-30 19:07:40 +0300

    add lab_3.test

---
+++ Lab_3/Test/IOHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using App;
+namespace Test;
+
+public class IOHandlerTest
+{
+    [Fact]
+    public void ReadMatrix_MatrixSizeOutOfRange_ThrowsException()
+    {
+        var filePath = Path.Combine("TestData", "test_matrix_size_out_of_max_size");
+
+        Assert.Throws<Exception>(() => IOHandler.ReadMatrixFromFile(filePath));
+    }
+
+    [Fact]
+    public void ReadMatrix()
+    {
+        var expectedResult = new int[,]
+        {
+            {1, 2, 3, 4 },
+            {5, 6 , 7, 8 },
+            {9, 10 , 11 , 12 },
+            {13, 14 , 15 , 16 }
+        };
+
+        var filePath = Path.Combine("TestData", "test_reading.txt");
+        var actualResult = IOHandler.ReadMatrixFromFile(filePath);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
