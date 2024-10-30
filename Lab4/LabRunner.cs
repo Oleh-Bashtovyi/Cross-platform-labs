8ed0e2f3e0c6a78b75f8c3d4c276f54126efc940
Oleh <olehbashtovyi@gmail.com>
2024-10-30 19:02:58 +0200

    add Lab4

---
+++ Lab4/LabRunner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4;
+
+public class LabRunner
+{
+    public void RunLab1(string inputFile, string outputFile)
+    {
+        try
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var orders = Lab_1.IOHandler.ReadOrders(inputFile);
+            var result = Lab_1.OrdersProblemSolver.Solve(orders);
+            Lab_1.IOHandler.WriteResult(result, outputFile);
+            Console.WriteLine("LAB #1");
+            Console.WriteLine("Input data:");
+            Console.WriteLine(string.Join(Environment.NewLine, orders).Trim());
+            Console.WriteLine("Output data:");
+            Console.WriteLine(result);
+            Console.WriteLine($"Result successfuly written to: {outputFile}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    public void RunLab2(string inputFile, string outputFile)
+    {
+        try
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var productBlocks = Lab_2.IOHandler.ReadProductBlocks(inputFile);
+            var result = Lab_2.BlocksCombiningProblemSolver.Solve(productBlocks.ToArray());
+            Lab_2.IOHandler.WriteResult(result, outputFile);
+            Console.WriteLine("LAB #2");
+            Console.WriteLine("Input data:");
+            Console.WriteLine(string.Join(Environment.NewLine, productBlocks).Trim());
+            Console.WriteLine("Output data:");
+            Console.WriteLine(result);
+            Console.WriteLine($"Result successfuly written to: {outputFile}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+
+    public void RunLab3(string inputFile, string outputFile)
+    {
+        try
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var matrix = Lab_3.IOHandler.ReadMatrixFromFile(inputFile);
+            var result = Lab_3.Solution.Solve(matrix);
+            Lab_3.IOHandler.WriteMatrixToFile(result, outputFile);
+            Console.WriteLine("LAB #3");
+            Console.WriteLine("Input data:");
+            Console.WriteLine(string.Join(Environment.NewLine, matrix).Trim());
+            Console.WriteLine("Output data:");
+            Console.WriteLine(string.Join(Environment.NewLine, result).Trim());
+            Console.WriteLine($"Result successfuly written to: {outputFile}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Lab_4;
+using Lab_1;
+using Lab_2;
+using Lab_3;
+using McMaster.Extensions.CommandLineUtils;
+using Lab4;
+
+
+namespace Lab_4;
+
+
+
+
+[Command(Name = "Lab4", Description = "Console app for labs")]
+[Subcommand(typeof(VersionCommand), typeof(RunCommand), typeof(SetPathCommand))]
+class Program
+{
+    static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    private void OnExecute()
+    {
+        Console.WriteLine("Specify a command");
+    }
+
+    private void OnUnknownCommand(CommandLineApplication app)
+    {
+        Console.WriteLine("Unknown command. Use one of the following:");
+        Console.WriteLine(" - version: Displays app version and author");
+        Console.WriteLine(" - run: Run a specific lab");
+        Console.WriteLine(" - set-path: Set input/output path");
+    }
+}
+
+
+[Command(Name = "version", Description = "Display version information.")]
+class VersionCommand
+{
+    private void OnExecute(IConsole console)
+    {
+        console.WriteLine("Author: Bashtovyi Oleh");
+        console.WriteLine("Version: 1.0.0");
+    }
+}
+
+
+[Command(Name = "set-path", Description = "Set input/output path.")]
+class SetPathCommand
+{
+    [Option("-p|--path", "Set LAB_PATH environment variable", CommandOptionType.SingleValue)]
+    public string Path { get; set; }
+
+    private void OnExecute(IConsole console)
+    {
+        if (!string.IsNullOrEmpty(Path))
+        {
+            Environment.SetEnvironmentVariable("LAB_PATH", Path);
+            console.WriteLine($"LAB_PATH set to: {Path}");
+        }
+        else
+        {
+            console.WriteLine("Please specify a path.");
+        }
+    }
+}
+
+
+
+[Command(Name = "run", Description = "Run a specific lab")]
+class RunCommand
+{
+    [Argument(0, "lab", "Specify lab to run (lab1)")]
+    public string? Lab { get; set; }
+
+    [Option("-I|--input", "Input file", CommandOptionType.SingleValue)]
+    public string? InputFile { get; set; }
+
+    [Option("-o|--output", "Output file", CommandOptionType.SingleValue)]
+    public string? OutputFile { get; set; }
+
+
+    private void OnExecute(CommandLineApplication app, IConsole console)
+    {
+        if (string.IsNullOrEmpty(Lab))
+        {
+            console.WriteLine("Error: No lab specified.");
+            app.ShowHelp();
+            return;
+        }
+
+        var labPath = GetLabDirectory(Lab);
+
+        if (labPath == null)
+        {
+            Console.WriteLine($"Unknown lab '{Lab}'. Available labs: lab1, lab2, lab3");
+            return;
+        }
+
+        var inputPath = InputFile ?? Environment.GetEnvironmentVariable("LAB_PATH");
+        var outputPath = OutputFile ?? Path.Combine(labPath, "OUTPUT.TXT");
+
+        if (string.IsNullOrEmpty(inputPath))
+        {
+            inputPath = Path.Combine(labPath, "INPUT.TXT");
+        }
+
+        Console.WriteLine($"INPUT: '{inputPath}'");
+        Console.WriteLine($"OUTPUT: '{outputPath}'");
+
+        if (!File.Exists(inputPath))
+        {
+            console.WriteLine($"Error: Input file not found at path '{inputPath}'");
+            return;
+        }
+
+
+        var runner = new LabRunner();
+
+        switch (Lab.ToLower())
+        {
+            case "lab1":
+                runner.RunLab1(inputPath, outputPath);
+                break;
+            case "lab2":
+                runner.RunLab2(inputPath, outputPath);
+                break;
+            case "lab3":
+                runner.RunLab3(inputPath, outputPath);
+                break;
+            default:
+                Console.WriteLine("Unknown lab specified.");
+                break;
+        }
+    }
+
+    private string? GetLabDirectory(string labName)
+    {
+        string projectRoot = Directory.GetCurrentDirectory();
+
+
+        switch (labName.ToLower())
+        {
+            case "lab1":
+                return Path.Combine(projectRoot, "LAB4", "LAB1");
+            case "lab2":
+                return Path.Combine(projectRoot, "LAB4", "LAB2");
+            case "lab3":
+                return Path.Combine(projectRoot, "LAB4", "LAB3");
+            default:
+                return null; // або можна кинути виключення, якщо це бажано
+        }
+    }
+}
+
